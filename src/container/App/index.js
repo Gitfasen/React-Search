88c2659886cacb1740ef050dc1b2c8ba33ab5dc9
Andrey <panchenko.a91@gmail.com>
2018-09-30 17:13:59 +0300

    #2

--- src/container/App/index.js
+++ src/container/App/index.js
@@ -1,45 +1,24 @@
 import React, { Component } from 'react';
 import logo from '../../logo.svg';
+import list from '../../data';
 import ListFramework from '../../components/ListFramework';
 import './App.sass';
 
 class Index extends Component {
-  filterList (e){
-    var updatedList = this.state.initialItems;
-    updatedList = updatedList.filter((item) => {
-    for (let key in item) {
-        return item.name.toLowerCase().search(e.target.value.toLowerCase()) !== -1 || item.url.toLowerCase().search(e.target.value.toLowerCase()) !== -1;
-    }
-
-    });
-    this.setState({items: updatedList});
-  }
   constructor(props) {
       super(props);
       this.state = {
-          initialItems: [
-              { name: 'Backbone.js', url: 'http://documentcloud.github.io/backbone/'},
-              { name: 'AngularJS', url: 'https://angularjs.org/'},
-              { name: 'jQuery', url: 'http://jquery.com/'},
-              { name: 'Prototype', url: 'http://www.prototypejs.org/'},
-              { name: 'React', url: 'http://facebook.github.io/react/'},
-              { name: 'Ember', url: 'http://emberjs.com/'},
-              { name: 'Knockout.js', url: 'http://knockoutjs.com/'},
-              { name: 'Dojo', url: 'http://dojotoolkit.org/'},
-              { name: 'Mootools', url: 'http://mootools.net/'},
-              { name: 'Underscore', url: 'http://documentcloud.github.io/underscore/'},
-              { name: 'Lodash', url: 'http://lodash.com/'},
-              { name: 'Moment', url: 'http://momentjs.com/'},
-              { name: 'Express', url: 'http://expressjs.com/'},
-              { name: 'Koa', url: 'http://koajs.com/'},
-          ],
-          items: []
+          items: list
       };
   }
-  componentWillMount() {
-        this.setState({items: this.state.initialItems})
+  filterList = e => {
+      const valueText = e.target.value.toLowerCase();
+      const updatedList = list.filter((item) => item.name.toLowerCase().includes(valueText) || item.url.toLowerCase().includes(valueText));
+
+      this.setState({items: updatedList});
   }
   render() {
+    const { items } = this.state;
     return (
       <div className="app">
         <header className="header" style={{marginBottom: '20px'}}>
@@ -52,10 +31,10 @@ class Index extends Component {
                     type="text"
                     className="form-control"
                     placeholder="Search..."
-                    onChange={this.filterList.bind(this)}
+                    onChange={this.filterList}
                 />
             </div>
-            <ListFramework data={this.state.items}/>
+            <ListFramework data={items}/>
         </div>
       </div>
     );
