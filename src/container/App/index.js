fa7a171b7716098c626413ee2ba8974325ab1d0b
Alex Holub <toryshadow@gmail.com>
2018-09-30 16:40:53 +0300

    Update index.js

--- src/container/App/index.js
+++ src/container/App/index.js
@@ -3,43 +3,41 @@ import logo from '../../logo.svg';
 import ListFramework from '../../components/ListFramework';
 import './App.sass';
 
-class Index extends Component {
-  filterList (e){
-    var updatedList = this.state.initialItems;
-    updatedList = updatedList.filter((item) => {
-    for (let key in item) {
-        return item.name.toLowerCase().search(e.target.value.toLowerCase()) !== -1 || item.url.toLowerCase().search(e.target.value.toLowerCase()) !== -1;
-    }
+const initialItems = [
+  { name: 'Backbone.js', url: 'http://documentcloud.github.io/backbone/'},
+  { name: 'AngularJS', url: 'https://angularjs.org/'},
+  { name: 'jQuery', url: 'http://jquery.com/'},
+  { name: 'Prototype', url: 'http://www.prototypejs.org/'},
+  { name: 'React', url: 'http://facebook.github.io/react/'},
+  { name: 'Ember', url: 'http://emberjs.com/'},
+  { name: 'Knockout.js', url: 'http://knockoutjs.com/'},
+  { name: 'Dojo', url: 'http://dojotoolkit.org/'},
+  { name: 'Mootools', url: 'http://mootools.net/'},
+  { name: 'Underscore', url: 'http://documentcloud.github.io/underscore/'},
+  { name: 'Lodash', url: 'http://lodash.com/'},
+  { name: 'Moment', url: 'http://momentjs.com/'},
+  { name: 'Express', url: 'http://expressjs.com/'},
+  { name: 'Koa', url: 'http://koajs.com/'},
+];
 
-    });
+class Index extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+        items: initialItems,
+    };
+  }
+  
+  filterList = e => {
+    const valueText = e.target.value.toLowerCase()
+    const updatedList = initialItems.filter((item) => item.name.toLowerCase().includes(valueText) || item.url.toLowerCase().includes(valueText));
+    
     this.setState({items: updatedList});
   }
-  constructor(props) {
-      super(props);
-      this.state = {
-          initialItems: [
-              { name: 'Backbone.js', url: 'http://documentcloud.github.io/backbone/'},
-              { name: 'AngularJS', url: 'https://angularjs.org/'},
-              { name: 'jQuery', url: 'http://jquery.com/'},
-              { name: 'Prototype', url: 'http://www.prototypejs.org/'},
-              { name: 'React', url: 'http://facebook.github.io/react/'},
-              { name: 'Ember', url: 'http://emberjs.com/'},
-              { name: 'Knockout.js', url: 'http://knockoutjs.com/'},
-              { name: 'Dojo', url: 'http://dojotoolkit.org/'},
-              { name: 'Mootools', url: 'http://mootools.net/'},
-              { name: 'Underscore', url: 'http://documentcloud.github.io/underscore/'},
-              { name: 'Lodash', url: 'http://lodash.com/'},
-              { name: 'Moment', url: 'http://momentjs.com/'},
-              { name: 'Express', url: 'http://expressjs.com/'},
-              { name: 'Koa', url: 'http://koajs.com/'},
-          ],
-          items: []
-      };
-  }
-  componentWillMount() {
-        this.setState({items: this.state.initialItems})
-  }
+
   render() {
+    const { items } = this.state;
+
     return (
       <div className="app">
         <header className="header" style={{marginBottom: '20px'}}>
@@ -52,10 +50,10 @@ class Index extends Component {
                     type="text"
                     className="form-control"
                     placeholder="Search..."
-                    onChange={this.filterList.bind(this)}
+                    onChange={this.filterList}
                 />
             </div>
-            <ListFramework data={this.state.items}/>
+            <ListFramework data={items}/>
         </div>
       </div>
     );
